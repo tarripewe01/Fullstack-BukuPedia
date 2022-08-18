dcb27bfbbd652367f94f3cce8056326db5d6862b
tarripewe <tarripewe@umrah.ac.id>
2022-08-18 11:50:52 +0700

    Client: Header

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { Header } from "./components";
 import { Home, Login, Register } from "./pages";
 
 function App() {
   return (
     <BrowserRouter>
       <div>
+        <Header />
         <ToastContainer />
         <Routes>
           <Route path="/" element={<Home />} />
---
+++ client/src/components/Header.js
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+import {
+  MDBNavbar,
+  MDBContainer,
+  MDBIcon,
+  MDBNavbarLink,
+  MDBNavbarNav,
+  MDBNavbarItem,
+  MDBNavbarToggler,
+  MDBCollapse,
+  MDBNavbarBrand,
+} from "mdb-react-ui-kit";
+import { Colors } from "../utils/colors";
+import { useSelector } from "react-redux";
+
+const Header = () => {
+  const [show, setShow] = useState(false);
+  const { user } = useSelector((state) => ({ ...state.auth }));
+
+  return (
+    <MDBNavbar fixed="top" expand="lg" style={styles.header}>
+      <MDBContainer>
+        <MDBNavbarBrand href="/" style={styles.brand}>
+          BukuPedia
+        </MDBNavbarBrand>
+        <MDBNavbarToggler
+          type="button"
+          aria-expanded="false"
+          aria-label="Toogle Navigation"
+          onClick={() => setShow(!show)}
+          style={{ color: Colors.white }}
+        >
+          <MDBIcon fas icon="bars" />
+        </MDBNavbarToggler>
+        <MDBCollapse show={show} navbar>
+          <MDBNavbarNav right fullWidth={false} className="mb-2 mb-lg-0">
+            <MDBNavbarItem>
+              <MDBNavbarLink href="/">
+                <p className="header-text">Home</p>
+              </MDBNavbarLink>
+            </MDBNavbarItem>
+            {user?.result?._id && (
+              <>
+                <MDBNavbarItem>
+                  <MDBNavbarLink href="/addBook">
+                    <p className="header-text">Add Book</p>
+                  </MDBNavbarLink>
+                </MDBNavbarItem>
+                <MDBNavbarItem>
+                  <MDBNavbarLink href="/dashboard">
+                    <p className="header-text">Dashboard</p>
+                  </MDBNavbarLink>
+                </MDBNavbarItem>
+              </>
+            )}
+            {user?.result?._id ? (
+              <MDBNavbarItem>
+                <MDBNavbarLink href="/login">
+                  <p className="header-text">Logout</p>
+                </MDBNavbarLink>
+              </MDBNavbarItem>
+            ) : (
+              <MDBNavbarItem>
+                <MDBNavbarLink href="/login">
+                  <p className="header-text">Login</p>
+                </MDBNavbarLink>
+              </MDBNavbarItem>
+            )}
+          </MDBNavbarNav>
+        </MDBCollapse>
+      </MDBContainer>
+    </MDBNavbar>
+  );
+};
+
+export default Header;
+
+const styles = {
+  header: {
+    backgroundColor: Colors.primary,
+  },
+  brand: {
+    color: Colors.white,
+    fontWeight: "600",
+    fontSize: 22,
+  },
+};
